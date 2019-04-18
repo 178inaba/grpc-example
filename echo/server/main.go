b137ab9ccb0f4fb21ebbb06194d0df82f1759351
178inaba <178inaba@users.noreply.github.com>
2019-04-18 23:01:03 +0900

    Add echo/server

---
+++ echo/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/178inaba/grpc-example/echo/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := ":50051"
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+
+	srv := grpc.NewServer()
+	pb.RegisterEchoServiceServer(srv, &echoService{})
+	log.Printf("Start server on port%s\n", port)
+	if err := srv.Serve(lis); err != nil {
+		log.Printf("Failed to serve: %v\n", err)
+	}
+}
