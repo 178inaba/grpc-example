b62dc1fa5aeed5afacc57f3bf1d3bd846e6cbb56
178inaba <178inaba@users.noreply.github.com>
2019-04-23 00:19:37 +0900

    Add uploader/server

---
+++ uploader/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/178inaba/grpc-example/uploader/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := 50051
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v.", err)
+	}
+
+	srv := grpc.NewServer()
+	pb.RegisterFileServiceServer(srv, &fileService{})
+	log.Printf("Start server on port: %d.", port)
+	if err := srv.Serve(lis); err != nil {
+		log.Printf("Failed to serve: %v.", err)
+	}
+}
---
+++ uploader/server/service.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	pb "github.com/178inaba/grpc-example/uploader/proto"
+)
+
+type fileService struct{}
+
+func (s *fileService) Upload(stream pb.FileService_UploadServer) error {
+	var name string
+	var blob []byte
+	for {
+		c, err := stream.Recv()
+		if err == io.EOF {
+			log.Printf("Done %d bytes.", len(blob))
+			break
+		} else if err != nil {
+			return err
+		}
+
+		name = c.GetName()
+		blob = append(blob, c.GetData()...)
+	}
+
+	fp := filepath.Join("./uploader/resource", name)
+	ioutil.WriteFile(fp, blob, 0644)
+	stream.SendAndClose(&pb.FileResponse{Size: int64(len(blob))})
+
+	return nil
+}
