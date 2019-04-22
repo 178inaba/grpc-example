6bfcaf0d9316689804ce7b42931017132f654751
178inaba <178inaba@users.noreply.github.com>
2019-04-23 01:25:33 +0900

    Fix code format to downloader/server

--- downloader/server/service.go
+++ downloader/server/service.go
@@ -21,17 +21,14 @@ func (s *fileService) Download(req *pb.FileRequest, stream pb.FileService_Downlo
 	buf := make([]byte, 1000*1024)
 	for {
 		n, err := fs.Read(buf)
-		if err != nil {
-			if err != io.EOF {
-				return err
-			}
-			break
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
 		}
 
 		if err := stream.Send(&pb.FileResponse{Data: buf[:n]}); err != nil {
 			return err
 		}
 	}
-
-	return nil
 }
