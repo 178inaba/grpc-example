24ed6c1dfe00d464993f60e82992142cb7cb1aa4
178inaba <178inaba@users.noreply.github.com>
2019-05-02 01:14:13 +0900

    Add message tampering verification in metadata

--- echo/client/main.go
+++ echo/client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -24,10 +26,20 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	if len(os.Args) < 2 {
+		log.Fatal("Please enter a message.")
+	}
+
 	msg := os.Args[1]
 
-	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("k1", "v1-1", "k1", "v1-2", "k2", "v2"))
-	r, err := client.Echo(ctx, &pb.EchoRequest{Message: msg})
+	h := sha256.New()
+	h.Write([]byte(msg))
+	hash := fmt.Sprintf("%x", h.Sum(nil))
+	log.Printf("Message sha256 is %q.", hash)
+
+	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("k1", "v1-1", "k1", "v1-2", "k2", "v2", "sha256", hash))
+	var header, trailer metadata.MD
+	r, err := client.Echo(ctx, &pb.EchoRequest{Message: msg}, grpc.Header(&header), grpc.Trailer(&trailer))
 	if err != nil {
 		st, ok := status.FromError(err)
 		if ok {
@@ -37,5 +49,13 @@ func main() {
 		log.Fatalf("Did not echo: %v.", err)
 	}
 
-	log.Println(r.GetMessage())
+	serverHash := trailer.Get("sha256")[0]
+	log.Printf("client hash is %q.", hash)
+	log.Printf("server hash is %q.", serverHash)
+	if serverHash != hash {
+		log.Fatal("The message has been tampered with.")
+	}
+
+	log.Printf("Server echo header metadata: %v.", header)
+	log.Printf("Server echo message is %q.", r.GetMessage())
 }
--- echo/server/service.go
+++ echo/server/service.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"fmt"
+	"log"
 
 	pb "github.com/178inaba/grpc-example/echo/proto"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 )
 
@@ -16,5 +21,28 @@ func (s *echoService) Echo(ctx context.Context, req *pb.EchoRequest) (*pb.EchoRe
 		return nil, status.Error(codes.InvalidArgument, "message is empty")
 	}
 
+	log.Printf("Message is %q.", msg)
+
+	h := sha256.New()
+	h.Write([]byte(msg))
+	hash := fmt.Sprintf("%x", h.Sum(nil))
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok {
+		log.Printf("Metadata is %v.", md)
+
+		clientHash := md.Get("sha256")[0]
+		log.Printf("client message hash is %q.", clientHash)
+		log.Printf("server message hash is %q.", hash)
+		if clientHash != hash {
+			return nil, status.Error(codes.InvalidArgument, "the message has been tampered with")
+		}
+
+		grpc.SendHeader(ctx, md)
+	} else {
+		return nil, status.Error(codes.InvalidArgument, "sha256 not found in header")
+	}
+
+	grpc.SetTrailer(ctx, metadata.Pairs("sha256", hash))
 	return &pb.EchoResponse{Message: msg}, nil
 }
